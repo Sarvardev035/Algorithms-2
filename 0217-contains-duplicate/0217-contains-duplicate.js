41e706738749e691ec197de23f8fdfcd5f558736
Sarvarbek Sharipboyev <sarvarbek.sharipboyev1@s.amity.edu>
2026-02-08 11:47:47 +0500

    Time: 13 ms (89.27%), Space: 72.4 MB (87.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const seen = new Set();
+    for (const num of nums) {
+        if (seen.has(num)){
+            return true;
+        }
+        seen.add(num);
+    }
+    
+        return false;
+        
+
+ 
+};
